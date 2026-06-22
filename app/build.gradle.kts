90d726434888f75c79f68de43d9c63ac8bf8430b
Summer-DeZ <295557102+Summer-DeZ@users.noreply.github.com>
2026-06-22 16:24:08 +0800

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "io.github.summerdez.asmrplayer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
